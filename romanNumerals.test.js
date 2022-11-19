e42f7024c1b8da18393cd347f30174c113d94b0b
Shiraz Esat <shiraz.esat@gmail.com>
2022-11-19 19:51:19 +0000

    Provide more TDD tests
    Also function has been updated to get tests passing
    red-green-refactor used

--- romanNumerals.test.js
+++ romanNumerals.test.js
@@ -4,4 +4,34 @@ describe("getRomanNumeral", () => {
   it("should return I when 1 is supplied", () => {
     expect(getRomanNumeral(1)).toBe("I");
   });
+  it("should return II when 2 is supplied", () => {
+    expect(getRomanNumeral(2)).toBe("II");
+  });
+  it("should return V when 5 is supplied", () => {
+    expect(getRomanNumeral(5)).toBe("V");
+  });
+  it("should return VIII when 8 is supplied", () => {
+    expect(getRomanNumeral(6)).toBe("VI");
+  });
+  it("should return VIII when 8 is supplied", () => {
+    expect(getRomanNumeral(8)).toBe("VIII");
+  });
+  it("should return IX when 9 is supplied", () => {
+    expect(getRomanNumeral(9)).toBe("IX");
+  });
+  it("should return X when 10 is supplied", () => {
+    expect(getRomanNumeral(10)).toBe("X");
+  });
+  it("should return XIII when 13 is supplied", () => {
+    expect(getRomanNumeral(13)).toBe("XIII");
+  });
+  it("should return XIV when 14 is supplied", () => {
+    expect(getRomanNumeral(14)).toBe("XIV");
+  });
+  it("should return XX when 20 is supplied", () => {
+    expect(getRomanNumeral(20)).toBe("XX");
+  });
+  it("should return XXXVIII when 38 is supplied", () => {
+    expect(getRomanNumeral(38)).toBe("XXXVIII");
+  });
 });
--- withTDD.js
+++ withTDD.js
@@ -1,3 +1,11 @@
 export function getRomanNumeral(n) {
-  if (n == 1) return "I";
+  if (n > 10) return "X" + getRomanNumeral(n - 10);
+  if (n == 10) return "X";
+  if (n == 9) return "IX";
+
+  if (n > 5) return "V" + getRomanNumeral(n - 5);
+  if (n == 5) return "V";
+  if (n == 4) return "IV";
+
+  return "I".repeat(n);
 }
